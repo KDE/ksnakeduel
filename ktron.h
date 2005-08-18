3f506135cefe5bae88d95971f8b30f5bdad2195a
Benjamin Meyer <ben@meyerhome.net>
2005-08-18 11:17:28 +0000

    Update copyright
    svn path=/trunk/KDE/kdegames/ktron/; revision=450509

--- ktron.h
+++ ktron.h
@@ -2,7 +2,8 @@
   This file is part of the game 'KTron'
 
   Copyright (C) 1998-2000 by Matthias Kiefer <matthias.kiefer@gmx.de>
-
+  Copyright (C) 2005 Benjamin C. Meyer <ben at meyerhome dot net> 
+  
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
--- player.cpp
+++ player.cpp
@@ -2,6 +2,7 @@
   This file is part of the game 'KTron'
 
   Copyright (C) 1998-2000 by Matthias Kiefer <matthias.kiefer@gmx.de>
+  Copyright (C) 2005 Benjamin C. Meyer <ben at meyerhome dot net>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
