8561d1a9c4247b87ae2309fc3a6386f9a8ab2c4e
David Faure <faure@kde.org>
2007-01-23 14:46:02 +0000

    kill module-global config.h
    svn path=/trunk/KDE/kdegames/ktron/; revision=626506

--- ktron.h
+++ ktron.h
@@ -2,8 +2,8 @@
   This file is part of the game 'KTron'
 
   Copyright (C) 1998-2000 by Matthias Kiefer <matthias.kiefer@gmx.de>
-  Copyright (C) 2005 Benjamin C. Meyer <ben at meyerhome dot net> 
-  
+  Copyright (C) 2005 Benjamin C. Meyer <ben at meyerhome dot net>
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
@@ -22,8 +22,6 @@
 
 #ifndef KTRON_H
 #define KTRON_H
-
-#include <config.h>
 
 #include <kmainwindow.h>
 #include "tron.h"
@@ -57,7 +55,7 @@ private slots:
   void loadSettings();
   /** updates players points in statusbar and checks if someone has won */
   void changeStatus(Player);
-  
+
   void showWinner(Player winner);
   void showSettings();
 };
--- tron.h
+++ tron.h
@@ -8,22 +8,20 @@
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-  *******************************************************************************/  
+  *******************************************************************************/
 
 #ifndef TRON_H
 #define TRON_H
-
-#include <config.h>
 
 #include <QWidget>
 #include <QPixmap>
@@ -156,7 +154,7 @@ private:
   * -1 means update both players
   */
   void updateDirections(int playerNr=-1);
-  
+
 private slots:
     /**
     * This is the main function of KTron.
