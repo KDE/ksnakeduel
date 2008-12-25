5f91fbd671986336d9af94b9e127b827e070294e
Stas Verberkt <kde@legolasweb.nl>
2008-12-25 15:51:00 +0000

    Base for abstraction and LibKdeGames in find_package
    svn path=/trunk/playground/games/ktron/; revision=901470

---
+++ object.h
@@ -0,0 +1,44 @@
+/* ********************************************************************************
+  This file is part of the kde-game 'KTron'
+
+  Copyright (C) 1998-2000 by Matthias Kiefer <matthias.kiefer@gmx.de>
+  Copyright (C) 2005 Benjamin C. Meyer <ben at meyerhome dot net>
+  Copyright (C) 2008 Stas Verberkt <legolas at legolasweb dot nl>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+  *******************************************************************************/  
+
+#include <QString>
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+/**
+* @short This class represents a drawable object on the playfield
+*/
+class Object
+{
+	public:
+		Object();
+		void setCoordinates(int x, int y);
+		QString getSVGName();
+
+	private:
+		int xCoordinate, yCoordinate;
+		QString svgName;
+};
+
+#endif // OBJECT_H
