698904aef606d62eb487d6f49f8fa5352d434213
Montel Laurent <montel@kde.org>
2014-12-20 09:49:58 +0100

    Fix install icons

--- ktron.cpp
+++ ktron.cpp
@@ -286,7 +286,7 @@ void KTron::showHighscores() {
  * Close KTron
  */
 void KTron::close() {
-	Settings::self()->writeConfig();
+	Settings::self()->save();
 }
 
 void KTron::closeEvent(QCloseEvent *event)
