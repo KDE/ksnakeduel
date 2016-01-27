d60063baadc163084bdf49266be3eca1c8f586b5
Frederik Schwarzer <schwarzerf@gmail.com>
2016-01-27 12:54:41 +0100

    Use KCrash.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 
 #include <KLocalizedString>
 #include <KAboutData>
+#include <KCrash>
 #include <Kdelibs4ConfigMigrator>
 #include <QCommandLineParser>
 #include <QCommandLineOption>
@@ -54,41 +55,42 @@ int main(int argc, char* argv[])
     migrate.setUiFiles(QStringList() << QStringLiteral("ksnakeduelui.rc"));
     migrate.migrate();
 
-  KAboutData aboutData( QStringLiteral("ksnakeduel"), i18n("KSnakeDuel"),
-    KTRON_VERSION, description, KAboutLicense::GPL, notice);
-  aboutData.addAuthor(i18n("Matthias Kiefer"), i18n("Original author"), QStringLiteral("matthias.kiefer@gmx.de"));
-  aboutData.addAuthor(i18n("Benjamin Meyer"), i18n("Various improvements"), QStringLiteral("ben+ktron@meyerhome.net"));
-  aboutData.addAuthor(i18n("Stas Verberkt"), i18n("KDE 4 Port, interface revision and KSnake mode"), QStringLiteral("legolas@legolasweb.nl"));
+    KAboutData aboutData( QStringLiteral("ksnakeduel"), i18n("KSnakeDuel"),
+            KTRON_VERSION, description, KAboutLicense::GPL, notice);
+    aboutData.addAuthor(i18n("Matthias Kiefer"), i18n("Original author"), QStringLiteral("matthias.kiefer@gmx.de"));
+    aboutData.addAuthor(i18n("Benjamin Meyer"), i18n("Various improvements"), QStringLiteral("ben+ktron@meyerhome.net"));
+    aboutData.addAuthor(i18n("Stas Verberkt"), i18n("KDE 4 Port, interface revision and KSnake mode"), QStringLiteral("legolas@legolasweb.nl"));
 
     QCommandLineParser parser;
     KAboutData::setApplicationData(aboutData);
+    KCrash::initialize();
     parser.addVersionOption();
     parser.addHelpOption();
-  parser.addOption(QCommandLineOption(QStringList() <<  QStringLiteral("snake"), i18n("Start in KSnake mode")));
+    parser.addOption(QCommandLineOption(QStringList() <<  QStringLiteral("snake"), i18n("Start in KSnake mode")));
 
     aboutData.setupCommandLine(&parser);
     parser.process(app);
     aboutData.processCommandLine(&parser);
 
 
-  //KStandardDirs::locateLocal("appdata", QLatin1String( "themes/" ));
+    //KStandardDirs::locateLocal("appdata", QLatin1String( "themes/" ));
 
-  if (parser.isSet(QStringLiteral("snake")))
-  {
-    Settings::setGameType(Settings::EnumGameType::Snake);
-  }
-  else if (Settings::gameType() == Settings::EnumGameType::Snake)
-  {
-    Settings::setGameType(Settings::EnumGameType::PlayerVSComputer);
-  }
+    if (parser.isSet(QStringLiteral("snake")))
+    {
+        Settings::setGameType(Settings::EnumGameType::Snake);
+    }
+    else if (Settings::gameType() == Settings::EnumGameType::Snake)
+    {
+        Settings::setGameType(Settings::EnumGameType::PlayerVSComputer);
+    }
 
-  Renderer::self(); // Creates Renderer
+    Renderer::self(); // Creates Renderer
 
-  KTron *ktron = new KTron();
-  ktron->show();
+    KTron *ktron = new KTron();
+    ktron->show();
 
-  app.setWindowIcon(QIcon::fromTheme(QStringLiteral("ksnakeduel")));
+    app.setWindowIcon(QIcon::fromTheme(QStringLiteral("ksnakeduel")));
 
-  return app.exec();
+    return app.exec();
 }
 
