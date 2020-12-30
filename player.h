9755c46f971bf5ad8f9eb718ff129de8fb96f11f
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-30 19:45:19 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- player.h
+++ player.h
@@ -1,25 +1,13 @@
-/**********************************************************************************
-  This file is part of the game 'KTron'
+/*
+    This file is part of the game 'KTron'
 
-  Copyright (C) 1998-2000 by Matthias Kiefer <matthias.kiefer@gmx.de>
-  Copyright (C) 2005 Benjamin C. Meyer <ben at meyerhome dot net>
-  Copyright (C) 2008-2009 Stas Verberkt <legolas at legolasweb dot nl>
+    SPDX-FileCopyrightText: 1998-2000 Matthias Kiefer <matthias.kiefer@gmx.de>
+    SPDX-FileCopyrightText: 2005 Benjamin C. Meyer <ben at meyerhome dot net>
+    SPDX-FileCopyrightText: 2008-2009 Stas Verberkt <legolas at legolasweb dot nl>
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+    SPDX-License-Identifier: GPL-2.0-or-later
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-  *******************************************************************************/
+*/
 
 #ifndef PLAYER_H
 #define PLAYER_H
